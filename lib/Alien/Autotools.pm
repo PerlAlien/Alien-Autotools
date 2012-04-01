d91bfa60f0590fe9c20be3252181a06eafe0efd7
Richard Simões <rsimoes@cpan.org>
2012-03-31 20:13:58 -0500

    stub pod, fill out Build.PL

--- lib/Alien/Autotools.pm
+++ lib/Alien/Autotools.pm
@@ -5,31 +5,8 @@ use warnings FATAL => "all";
 use utf8;
 
 # VERSION
-# ABSTRACT:
+# ABSTRACT: Build and install the GNU build system
+
+
 
 1;
-
-__END__
-
-=pod
-
-=encoding utf8
-
-=head1 NAME
-
-Alien::Autotools -
-
-=head1 SYNOPSIS
-
-=head1 DESCRIPTION
-
-=head1 AUTHOR
-
-Richard Simões C<< <rsimoes AT cpan DOT org> >>
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright © 2012 Richard Simões. This module is released under the terms of the
-L<GNU Lesser General Public License v. 3.0|http://gnu.org/licenses/lgpl.html>
-and may be modified and/or redistributed under the same or any compatible
-license.
